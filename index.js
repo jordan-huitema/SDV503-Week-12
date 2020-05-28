cc307ecb9e86195174a494ad9688e96f7613972d
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-05-29 10:55:35 +1200

    added classes examples and calculator example

--- index.js
+++ index.js
@@ -76,3 +76,64 @@ let hashTable = new HashTable()
 console.log(hashTable)
 
 
+//classes
+// A class is a template that can be applied to an object
+
+let firstNm = ["Jordan"]
+let lastNm = ["Huitema"]
+let year = [1998]
+class Student {
+    constructor(firstName,lastName, year, age){ //constructors are special functions that run when the class is applied to a object
+        this.firstName = firstName
+        this.lastName = lastName
+        this.year = year
+        this.age = age
+    }
+    print(input){                               //functions do not need to be declared within a class, they are... 'assumed' in a sense
+                                                //functions are called methods when inside a class
+        console.log(input)
+    }
+    studentAge(){
+        return `${this.firstName} is ${2020 - this.year} years old`
+    }
+    fullName(){
+        return `Full name is ${this.firstName} ${this.lastName}`
+    }
+    email(){
+        return `Email is ${this.firstName.toLowerCase()}-${this.lastName.toLowerCase()}@gmail.com`
+    }
+}
+
+let students = new Student(firstNm,lastNm,year)     //applys Students class template to students obj and uses firstNms and lastNms as data to input
+console.log(students)
+
+students.print("Beans")                                 //you can call a func within a obj the same way you would access a property 
+
+let studentsTwo = new Student("Ali","Kahwaji", 1993)
+console.log(studentsTwo)
+
+console.log(studentsTwo.studentAge())
+console.log(studentsTwo.fullName())
+console.log(studentsTwo.email())
+
+//calculator
+
+class Calc {
+    add(a,b){
+        return a + b
+    }
+    sub(a,b){
+        return a - b
+    }
+    mul(a,b){
+        return a*b
+    }
+    div(a,b){
+        return a/b
+    }
+}
+let calc = new Calc
+console.log(calc.add(10,5))
+console.log(calc.sub(10,5))
+console.log(calc.mul(10,5))
+console.log(calc.div(10,5))
